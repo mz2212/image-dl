68fa1a47461995f1f3acdf8da5885df62d2aab2a
Mike <michaelpulliam1@gmail.com>
2020-05-26 19:03:19 -0600

    Add a couple more sites, notes on duplication

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,9 @@ mod parser;
 mod booru;
 mod gelbooru;
 
+// ffs I'm duplicating a project: https://bionus.github.io/imgbrd-grabber/
+// Just use that instead. *cries in spagett code*
+
 fn main() {
     let input = env::args().last().unwrap();
     let url = Url::parse(input.as_str()).unwrap();
@@ -24,6 +27,15 @@ fn main() {
         //"derpibooru.org" => derpi::get_image_links(url), // going to need a custom one for derpi too
         "flandre.moe" => parser::get_image_links(url, "directlink"), // Also lolibooru.moe?
         "lolibooru.moe" => parser::get_image_links(url, "directlink"), // These two are a bit odd, one seems to direct to the other
+        "konachan.com" => parser::get_image_links(url, "directlink"), // kona's another easy one
+        "konachan.net" => parser::get_image_links(url, "directlink"),
+        //"kusubooru.com" => kusu::get_image_links(url), // woo custom parsers
+        "evbooru.com" => parser::get_image_links(url, "directlink"),
+        //"behoimi.org" => behoimi::get_image_links(url), // :eyeroll: moar custom parsers
+        //"www.bittersweetcandybowl.com" => candy::get_image_links(url), // I need to find a way to genericify these, they're mostly the same...
+        "e-shuushuu.net" => parser::get_image_links(url, "thumb_image"), // Dunno what's with the naming, it's a directlink
+        //"www.furaffinity.net" => furaffinity::get_image_links(url), // bugger, thought this one was going to be easy.
+        //"safebooru.org" => safebooru::get_image_links(url), // Literally runs gelbooru, like most of the customs, but with slightly different tags/url scheming
         _ => panic!("Unsupported site"),
     };
 
